a7211e1827d176ea6570d0f7157c7d03345b7421
Klied <zach.umali@gmail.com>
2024-01-15 15:04:46 +0800

    done

--- 4. Functions and Exports/index.js
+++ 4. Functions and Exports/index.js
@@ -1,39 +1,72 @@
-/*
-    Resources:
-    - MDN on Functions: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
-    - JavaScript Tutorial on Functions: https://javascript.info/function-basics
-    - freeCodeCamp on Functions: https://www.freecodecamp.org/news/what-is-a-function-javascript-function-examples/
-*/
-
-// Todo 4.1 Declare a function named 'greet' that takes one parameter 'name' and displays a greeting
-// Then, Call the 'greet' function with your name as the argument
-// Your code here
-
-// Checkpoint 4.1 What is the difference between a function declaration and a function expression?
-// Answer: 
-
-// Todo 4.2 Declare a function expression named 'calculateArea' that takes the length and width of a rectangle and returns its area
-// Then, Call the 'calculateArea' function with any length and width as arguments
-// Your code here
-
-// Checkpoint 4.2 What is a callback function? Provide an example.
-// Answer: 
-
-// Todo 4.3 Declare a higher-order function named 'modifyArray' that takes an array and a function as parameters. The function should modify each element in the array using the provided function and return the modified array.
-// Then, Call the 'modifyArray' function with the 'numbers' array and a function that increments each number by 1
-// Useful Tutorial: https://youtu.be/H4awPsyugS0?si=7wC1B7whXVkG8X5l
-// Your code here
-
-// Todo 4.4 See mathUtils.js for the instructions to create the add method and PI constant.
-// Then, import and call the add method here and display the value of the PI constant
-// Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
-// Your code here
-
-// Todo 4.5 See isPalindrome.js for the instructions to create the isPalindrome method.
-// Then, import and call the isPalindrome method here
-// Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
-// Your code here
-
-// Checkpoint 4.3 What does the require function and module.exports variable do?
-// Should we use it over the import and export keywords? Why? Research your answer.
-// Answer: 
+// Todo 4.1
+// Declare a function named 'greet' that takes one parameter 'name' and displays a greeting
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  // Call the 'greet' function with your name as the argument
+  greet("Your Name");
+  
+  // Checkpoint 4.1
+  // What is the difference between a function declaration and a function expression?
+  // Answer: Function declarations are hoisted, meaning they can be called before they are defined.
+  // Function expressions are not hoisted and must be defined before they are called.
+  
+  // Todo 4.2
+  // Declare a function expression named 'calculateArea' that takes the length and width of a rectangle and returns its area
+var calculateArea = function(length, width) {
+    return length * width;
+  }
+  
+  // Call the 'calculateArea' function with any length and width as arguments
+  var area = calculateArea(5, 10);
+  console.log("Area: " + area);
+  
+  
+  // Checkpoint 4.2
+  // What is a callback function? Provide an example.
+  // Answer: A callback function is a function passed as an argument to another function,
+  // which is then invoked inside the outer function. Example:
+  // setTimeout(function() {
+  //   console.log("Callback function executed after 2000 milliseconds");
+  // }, 2000);
+  
+  // Todo 4.3
+ // Declare a higher-order function named 'modifyArray' that takes an array and a function as parameters.
+// The function should modify each element in the array using the provided function and return the modified array.
+function modifyArray(array, callback) {
+    var modifiedArray = [];
+    for (var i = 0; i < array.length; i++) {
+      modifiedArray.push(callback(array[i]));
+    }
+    return modifiedArray;
+  }
+  
+  // Call the 'modifyArray' function with the 'numbers' array and a function that increments each number by 1
+  var numbers = [1, 2, 3, 4, 5];
+  var incrementedNumbers = modifyArray(numbers, function(num) {
+    return num + 1;
+  });
+  console.log("Incremented Numbers: " + incrementedNumbers);
+  
+  
+  // Todo 4.4
+  // Import and call the add method from mathUtils.js and display the value of the PI constant
+  const mathUtils = require('./mathUtils');
+  const sum = mathUtils.add(2, 3);
+  console.log("Sum:", sum);
+  console.log("PI Constant:", mathUtils.PI);
+  
+  // Todo 4.5
+  // Import and call the isPalindrome method from isPalindrome.js
+  const isPalindrome = require('./isPalindrome');
+  const result = isPalindrome("level");
+  console.log("Is Palindrome:", result);
+  
+  // Checkpoint 4.3
+  // What does the require function and module.exports variable do?
+  // Should we use it over the import and export keywords? Why? Research your answer.
+  // Answer: 'require' is used in CommonJS to import modules, and 'module.exports' is used to export variables,
+  // functions, or objects from a module. In modern JavaScript (ES6+), 'import' and 'export' keywords are preferred
+  // for module handling as they are part of the ECMAScript standard.
+  
--- 4. Functions and Exports/isPalindrome.js
+++ 4. Functions and Exports/isPalindrome.js
@@ -1,5 +1,12 @@
-// Todo 4.6a Create a funciton called isPalindrome function which checks if the given word is a palindrome
-// What is a Palindrome? See here: https://en.wikipedia.org/wiki/Palindrome. Feel free to search for the implementation
-
-// Todo 4.6b Set the isPalindrome function as the default export of the module
-// Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
+// Todo 4.6a Create a function called isPalindrome that checks if the given word is a palindrome
+function isPalindrome(word) {
+    // Convert the word to lowercase and remove any non-alphanumeric characters
+    var cleanWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+    // Reverse the cleanWord and compare it with the original cleanWord
+    return cleanWord === cleanWord.split('').reverse().join('');
+  }
+  
+  // Todo 4.6b Set the isPalindrome function as the default export of the module
+  module.exports = isPalindrome;
+  
